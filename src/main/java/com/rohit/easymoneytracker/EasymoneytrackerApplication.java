c44e7e630fc3eb48b9930a5ddc0f1b23852f4ee5
Rohit <rohiitnagar@gmail.com>
2026-02-11 23:18:10 +0530

    Initial commit

---
+++ src/main/java/com/rohit/easymoneytracker/EasymoneytrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.easymoneytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymoneytrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasymoneytrackerApplication.class, args);
+    }
+
+}
