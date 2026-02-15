7d233c07b7623829992389b20a3c23872dfb5b11
Rohit <rohiitnagar@gmail.com>
2026-02-15 15:49:04 +0530

    update

---
+++ src/main/java/com/rohit/easymoneytracker/entity/User.java
@@ -0,0 +1,43 @@
+package com.rohit.easymoneytracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Table(name= "users")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String mobile;
+    private String password;
+    private boolean enabled;
+
+    //Role Implementation
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public boolean setEnabled() {
+        return true;
+    }
+
+}
